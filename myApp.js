7cf94460ce7981c170f5a6ae5bdc8d6b0d683b9a
Ykaro Andrade <kaunego@hotmail.com>
2023-12-27 16:50:21 -0300

    p2 s3

--- myApp.js
+++ myApp.js
@@ -1,11 +1,23 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-
-
-
-
-
+app.get("/", (req, res) => {
+    res.send("Hello Express");
+  });
+  app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+  });
+  app.use("/public", express.static(__dirname + "/public"));
+  app.get("/json", (req, res) => {
+    res.json({
+      message: "Hello json"
+    });
+  });
+  if (process.env.VAR_NAME === "allCaps") {
+    response = "Hello World".toUpperCase();
+  } else {
+    response = "Hello World";
+  }
 
 
 
