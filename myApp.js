7465b427005abff63de357578540badfa30cae76
Ykaro Andrade <kaunego@hotmail.com>
2024-01-02 16:12:55 -0300

    one more

--- myApp.js
+++ myApp.js
@@ -1,3 +1,41 @@
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(function middleware(req, res, next) {
+  // Do something
+  // Call the next function in line:
+  next();
+});
+var string = req.method + " " + req.path + " - " + req.ip;
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({
+      time: req.time
+    });
+  }
+);
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word
+  });
+});
+app.get("/name", function(req, res) {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  // OR you can destructure and rename the keys
+  var { first: firstName, last: lastName } = req.query;
+  // Use template literals to form a formatted string
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
 let express = require('express');
 let app = express();
 console.log("Hello World");
@@ -18,7 +56,11 @@ app.get("/", (req, res) => {
   } else {
     response = "Hello World";
   }
-
+  app.post("/name", function(req, res) {
+    // Handle the data in the request
+    var string = req.body.first + " " + req.body.last;
+    res.json({ name: string });
+  });
 
 
 
